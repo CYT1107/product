c6938fa854a038c7ae104569c7856a12e5d6526c
CYT1107 <U10710036@go.utaipei.edu.tw>
2021-07-02 16:33:58 +0800

    finished_3

--- product.py
+++ product.py
@@ -17,3 +17,8 @@ print(products)
 #顯示products大清單內小清單p的各項
 for p in products:
     print(p[0], '的價格為', p[1])
+
+#寫入檔案
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
